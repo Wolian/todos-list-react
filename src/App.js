d52d89f0d029354284ff1339ac13c3f819849921
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-04-29 13:13:27 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -31,6 +31,12 @@ function App() {
     }));
   }
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task, done: true,
+    })))
+  }
+
 
   return (
     <Container className="container">
@@ -45,7 +51,7 @@ function App() {
       <Section
         title="Lista zadań"
         body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} setAllDone={setAllDone} />}
       />
 
     </Container>

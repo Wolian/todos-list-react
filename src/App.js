0c0ab5c9434ae43189b022d6dcea01c28918baf3
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-04-29 13:32:53 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -37,6 +37,17 @@ function App() {
     })))
   }
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
 
   return (
     <Container className="container">
@@ -45,7 +56,7 @@ function App() {
       />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section

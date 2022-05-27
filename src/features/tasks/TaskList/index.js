7c10ebf1a56b428ad6d8cb2d0be68b35b6d93656
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-05-27 15:55:23 +0200

    application split into features

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { List, Item, Content, Button } from "./styled";
 
-const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <List>
         {tasks.map(task => (
             <Item key={task.id} hidden={task.done && hideDone}>
@@ -20,4 +20,4 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     </List>
 );
 
-export default Tasks;
+export default TaskList;

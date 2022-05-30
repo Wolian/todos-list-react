937d8dac3584ace48ccd6ab6a52a43fab44db639
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-05-30 14:23:34 +0200

    tasks from react to redux updated

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,19 +1,29 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleHideDone, setAllDone, selectAreTasksEmpty, selectHideDone, selectIsEveryTaskDone } from "../tasksSlice";
 import { Wrapper, Button } from "./styled";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    <Wrapper>
-        {tasks.length > 0 && (
-            <React.Fragment>
-                <Button onClick={toggleHideDone}>
-                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
-                </Button>
-                <Button onClick={setAllDone} disabled={tasks.every(({ done }) => done)} >
-                    Ukończ wszystkie
-                </Button>
-            </React.Fragment>
-        )}
-    </Wrapper>
-);
+const Buttons = () => {
+    const areTasksEmpty = useSelector(selectAreTasksEmpty);
+    const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
+    const hideDone = useSelector(selectHideDone);
+
+    const dispatch = useDispatch();
+
+    return (
+        <Wrapper>
+            {!areTasksEmpty && (
+                <React.Fragment>
+                    <Button onClick={() => dispatch(toggleHideDone())}>
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                    </Button>
+                    <Button onClick={() => dispatch(setAllDone())} disabled={isEveryTaskDone} >
+                        Ukończ wszystkie
+                    </Button>
+                </React.Fragment>
+            )}
+        </Wrapper>
+    )
+};
 
 export default Buttons;
--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,23 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { selectHideDone, selectTasks, toggleTaskDone, removeTask } from "../tasksSlice";
 import { List, Item, Content, Button } from "./styled";
 
-const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <List>
-        {tasks.map(task => (
-            <Item key={task.id} hidden={task.done && hideDone}>
+const TaskList = () => {
+    const tasks = useSelector(selectTasks);
+    const hideDone = useSelector(selectHideDone);
 
-                <Button toggleDone onClick={() => toggleTaskDone(task.id)}>
-                    {task.done ? "✔" : ""}
-                </Button>
-                <Content done={task.done}>
-                    {task.content}
-                </Content>
-                <Button remove onClick={() => removeTask(task.id)}>
-                    🗑
-                </Button>
-            </Item>
-        ))}
-    </List>
-);
+    const dispatch = useDispatch();
+    return (
+        <List>
+            {tasks.map(task => (
+                <Item key={task.id} hidden={task.done && hideDone}>
+
+                    <Button toggleDone onClick={() => dispatch(toggleTaskDone(task.id))}>
+                        {task.done ? "✔" : ""}
+                    </Button>
+                    <Content done={task.done}>
+                        {task.content}
+                    </Content>
+                    <Button remove onClick={() => dispatch(removeTask(task.id))}>
+                        🗑
+                    </Button>
+                </Item>
+            ))}
+        </List>
+    )
+};
 
 export default TaskList;
